471eb79f57ad9dd1505fb8cacf70a258624940da
Stina Persson <stpe1901@student.miun.se>
2025-04-15 17:06:07 +0200

    Skapat controller och view med ett hello world

--- src/main/java/com/example/test_spring_boot/HomeController.java
+++ src/main/java/com/example/test_spring_boot/HomeController.java
@@ -1,9 +1,13 @@
 package com.example.test_spring_boot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class HomeController {
-
+    @RequestMapping("/") //för index-hemsidan (start)
+    public String home() {
+        return "index.html"; 
+    }
 }
